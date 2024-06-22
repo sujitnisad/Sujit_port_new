f3447966cc6798dc3e89195a4ea7e443740ffe36
Nisad <snisad30@massmutual.com>
2024-06-22 16:02:55 -0400

    thirdd

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -1,10 +1,135 @@
+import { useState } from "react";
 import "./Qualification.css";
 const Qualification = () => {
+  const [toggle, setToggle] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggle(index);
+  };
+
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
       <span className="section__subtitle">My Journey</span>
-      <div className="qualification__container"></div>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggle === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            Education
+          </div>
+
+          <div
+            className={
+              toggle === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggle === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Btech - Computer Science
+                </h3>
+                <span className="qualification__subtitle">
+                  Vardhaman College Of Engineering
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-schedule"></i> 2018 - 2022
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Intermediate - MPC</h3>
+                <span className="qualification__subtitle">
+                  Narayana Junior College
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-schedule"></i> 2016 - 2018
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Secondary School</h3>
+                <span className="qualification__subtitle">
+                  L.B.M high school
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-schedule"></i> 2006 - 2016
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={
+              toggle === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Frontend Development Analyst
+                </h3>
+                <span className="qualification__subtitle">
+                  Massmutual India
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-schedule"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
