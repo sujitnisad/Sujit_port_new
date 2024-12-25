6486925e6c6b29b2cb00d290ffd4f79d3e357483
samnisad <102129463+samnisad@users.noreply.github.com>
2024-12-25 17:16:34 +0530

    last

--- src/components/Skills/Backend.jsx
+++ src/components/Skills/Backend.jsx
@@ -1,13 +1,13 @@
 const Backend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Backend Skills</h3>
+      <h3 className="skills__title">Backend Basic Skills</h3>
       <div className="skills__box">
         <div className="skills__group">
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">Core Java</h3>
+              <h3 className="skills__name">Node.js</h3>
               <span className="skills__level">Basic</span>
             </div>
           </div>
@@ -15,7 +15,7 @@ const Backend = () => {
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">Hibernate</h3>
+              <h3 className="skills__name">Express.js</h3>
               <span className="skills__level">Basic</span>
             </div>
           </div>
@@ -23,15 +23,7 @@ const Backend = () => {
           <div className="skills__data">
             <i className="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">Spring Framework</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i className="bx bx-badge-check"></i>
-            <div>
-              <h3 className="skills__name">SQL</h3>
+              <h3 className="skills__name">MySQL</h3>
               <span className="skills__level">Basic</span>
             </div>
           </div>
